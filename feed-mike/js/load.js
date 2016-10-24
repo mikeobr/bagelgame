c956db5618c691b3fd106ce39e0027cc04d7785f
fangyu <fangyu@FANG-YUs-MacBook-Pro.local>
2016-10-24 17:15:16 -0400

    Change to use a smaller version of assets

--- feed-mike/js/load.js
+++ feed-mike/js/load.js
@@ -8,10 +8,10 @@ var loadState = {
   preload: function() {
     game.load.script("webfont", "//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js");
     game.load.image('sky', 'assets/night-sky.png');
-    game.load.image('mike', '../assets/hungry-mike.png');
-    game.load.image('baddie', '../assets/bad-broccoli.png');
+    game.load.image('mike', 'assets/hungry-mike-small.png');
+    game.load.image('baddie', 'assets/bad-broccoli-small.png');
     game.load.image('bagel', 'assets/bagel-small.png');
-    game.load.image('weapoon', 'assets/green-flame.png');
+    game.load.image('weapoon', 'assets/knife.png');
     game.load.image('baddieWeapoon', 'assets/flame.png');
     game.load.image('player', 'assets/rocket.png');
   },
