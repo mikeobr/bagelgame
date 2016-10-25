cfb639a763fcc39ea4c798c5c31feb9135604162
Shu Lin Chan <shulinchan@dhcp-172-16-43-59.crl.vecna.com>
2016-10-25 11:29:11 -0400

    Added go back to main menu function

--- dont-eat-vegetables/js/lose.js
+++ dont-eat-vegetables/js/lose.js
@@ -1,12 +1,21 @@
 var loseState = {
   create: function() {
     var loseLabel = game.add.text(80, 80, 'YOU LOST!', {font: '50px Arial', fill: '#ffffff'});
-    var startLabel = game.add.text(80, game.world.height-80, 'Press "R" to restart', {font: '20px Arial', fill: '#ffffff'});
+    var startLabel = game.add.text(80, game.world.height-160, 'Press "R" to restart', {font: '20px Arial', fill: '#ffffff'});
     var rKey = game.input.keyboard.addKey(Phaser.Keyboard.R);
     rKey.onDown.addOnce(this.restart, this);
+
+    var goToMainMenuLabel = game.add.text(80, game.world.height-80, 'Press "M" to go back to main menu', {font: '20px Arial', fill: '#ffffff'});
+    var mKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
+    mKey.onDown.addOnce(this.backToMenu, this);
   },
 
   restart: function() {
     game.state.start('play');
+  },
+
+  backToMenu: function() {
+  	var host = window.location.hostname;
+  	window.location.href = 'http://' + host + ':8080';
   },
 }
